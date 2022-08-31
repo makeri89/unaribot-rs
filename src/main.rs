e2fde64f72c28ba83004a5a554e9363828c65943
Markus Kaihola <markus.kaihola@helsinki.fi>
2022-08-31 17:15:51 +0300

    Added support for all unicafes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use teloxide::{prelude::*, utils::command::BotCommands, types::ParseMode, utils::markdown};
+use serde::{Deserialize, Serialize};
 use std::error::Error;
-use serde::{Serialize, Deserialize};
+use teloxide::{prelude::*, types::ParseMode, utils::command::BotCommands};
 
 static UNICAFE_BASE_URL: &str = "https://unicafe.fi/wp-json/swiss/v1/restaurants/?lang=fi";
 
@@ -9,13 +9,13 @@ struct MenuData {
     name: String,
     id: i32,
     areacode: i32,
-    menus: Vec<MenuList>
+    menus: Vec<MenuList>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct Price {
     name: String,
-    value: PriceOptions
+    value: PriceOptions,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -32,7 +32,7 @@ struct Menu {
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct MenuList {
-    data: Vec<Menu>
+    data: Vec<Menu>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -56,7 +56,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
     teloxide::commands_repl(bot, answer, Command::ty()).await;
 
     Ok(())
-
 }
 
 async fn get_restaurants() -> Result<Vec<Restaurant>, reqwest::Error> {
@@ -68,10 +67,25 @@ async fn get_restaurants() -> Result<Vec<Restaurant>, reqwest::Error> {
 
 fn get_index_by_name(name: &str) -> usize {
     match name {
+        "kaivopiha" => 0,
+        "wellterkko" => 1,
+        "wellkaisa" => 2,
+        "viikuna" => 3,
+        "sockom" => 4,
+        "rotunda" => 5,
+        "oliver" => 6,
+        "porthania" => 8,
         "physicum" => 9,
+        "pescovege" => 10,
+        "olivia" => 11,
+        "metsatalo" => 12,
+        "meilahti" => 13,
+        "infokeskus" => 14,
         "exactum" => 15,
         "chemicum" => 16,
-        _ => 0
+        "portaali" => 18,
+        "biokeskus" => 19,
+        _ => 0,
     }
 }
 
@@ -86,6 +100,36 @@ enum Command {
     Exactum,
     #[command(description = "Physicums menu")]
     Physicum,
+    #[command(description = "Kaivopihas menu")]
+    Kaivopiha,
+    #[command(description = "WELL Terkkos menu")]
+    WELLTerkko,
+    #[command(description = "WELL Kaisas menu")]
+    WELLKaisa,
+    #[command(description = "Viikunas menu")]
+    Viikuna,
+    #[command(description = "Sockoms menu")]
+    Sockom,
+    #[command(description = "Rotundas menu")]
+    Rotunda,
+    #[command(description = "Olivers menu")]
+    Oliver,
+    #[command(description = "Porthanias menu")]
+    Porthania,
+    #[command(description = "Pescoveges menu")]
+    Pescovege,
+    #[command(description = "Olivias menu")]
+    Olivia,
+    #[command(description = "Metsatalos menu")]
+    Metsatalo,
+    #[command(description = "Meilahtis menu")]
+    Meilahti,
+    #[command(description = "Infokeskus' menu")]
+    Infokeskus,
+    #[command(description = "Portaalis menu")]
+    Portaali,
+    #[command(description = "Biokeskus' menu")]
+    Biokeskus,
 }
 
 async fn answer(
@@ -95,29 +139,44 @@ async fn answer(
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
     match command {
         Command::Help => {
-            bot.send_message(message.chat.id, Command::descriptions().to_string()).await?
+            bot.send_message(message.chat.id, Command::descriptions().to_string())
+                .await?
         }
-        Command::Chemicum => {
-            let data = get_restaurants().await?;
-            let index = get_index_by_name("chemicum");
-            let res = format_message(&data[index].menuData.menus[0]);
-            bot.send_message(message.chat.id, format!("{}", res)).parse_mode(ParseMode::MarkdownV2).await?
-        }
-        Command::Exactum => {
-            let data = get_restaurants().await?;
-            let index = get_index_by_name("exactum");
-            let res = format_message(&data[index].menuData.menus[0]);
-            bot.send_message(message.chat.id, format!("{}", res)).parse_mode(ParseMode::MarkdownV2).await?
-        }
-        Command::Physicum => {
-            let data = get_restaurants().await?;
-            let index = get_index_by_name("physicum");
-            let res = format_message(&data[index].menuData.menus[0]);
-            bot.send_message(message.chat.id, format!("{}", res)).parse_mode(ParseMode::MarkdownV2).await?
-        }
+        Command::Chemicum => message_sender("chemicum", message, bot).await?,
+        Command::Exactum => message_sender("exactum", message, bot).await?,
+        Command::Physicum => message_sender("physicum", message, bot).await?,
+        Command::Kaivopiha => message_sender("kaivopiha", message, bot).await?,
+        Command::WELLTerkko => message_sender("wellterkko", message, bot).await?,
+        Command::WELLKaisa => message_sender("wellkaisa", message, bot).await?,
+        Command::Viikuna => message_sender("viikuna", message, bot).await?,
+        Command::Sockom => message_sender("sockom", message, bot).await?,
+        Command::Rotunda => message_sender("rotunda", message, bot).await?,
+        Command::Oliver => message_sender("oliver", message, bot).await?,
+        Command::Porthania => message_sender("porthania", message, bot).await?,
+        Command::Pescovege => message_sender("pescovege", message, bot).await?,
+        Command::Olivia => message_sender("olivia", message, bot).await?,
+        Command::Metsatalo => message_sender("metsatalo", message, bot).await?,
+        Command::Meilahti => message_sender("meilahti", message, bot).await?,
+        Command::Infokeskus => message_sender("infokeskus", message, bot).await?,
+        Command::Portaali => message_sender("portaali", message, bot).await?,
+        Command::Biokeskus => message_sender("biokeskus", message, bot).await?,
     };
 
     Ok(())
+}
+
+async fn message_sender(
+    name: &str,
+    message: Message,
+    bot: AutoSend<Bot>,
+) -> Result<teloxide::prelude::Message, Box<dyn Error + Send + Sync>> {
+    let data = get_restaurants().await?;
+    let index = get_index_by_name(name);
+    let res = format_message(&data[index].menuData.menus[0]);
+    Ok(bot
+        .send_message(message.chat.id, format!("{}", res))
+        .parse_mode(ParseMode::MarkdownV2)
+        .await?)
 }
 
 fn format_message(data: &MenuList) -> String {
@@ -156,8 +215,7 @@ fn format_for_markdown(message: String) -> String {
 fn format_food_and_price(data: &Menu) -> String {
     if data.name.starts_with("RAVINTOLA AVOINNA") {
         let times = data.name.split_whitespace();
-        let bolded = markdown::bold("Aukioloajat: ");
-        return format!("{}{}\n\n", bolded, times.last().unwrap());
+        return format!("Aukioloajat: {}\n\n", times.last().unwrap());
     }
     format!("{}, {}€\n", data.name, data.price.value.student)
 }
